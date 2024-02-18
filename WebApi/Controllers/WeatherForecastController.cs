2c6a406fa11949670250b7725e99e5bb800a52e4
Hendry Priyatno <hendrysan@users.noreply.github.com>
2024-02-18 22:23:33 +0700

    Update WeatherForecastController.cs

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
-    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
